91e413dff219bd1783bd65f86932b23b99470c55
rpobulic <rade.pobulic@gmail.com>
2017-05-13 22:18:18 +0200

    use current directory for dump files

--- bin/import_masked.sh
+++ bin/import_masked.sh
@@ -3,7 +3,7 @@ DIR=$(dirname $(dirname "$0"))
 dropdb --if-exists -iU $2 $1 
 createdb -U $2 -O $2 $1 
 psql -q $1 $2 <<EOF
-\i $DIR/sql/pre_data.sql
-\i $DIR/sql/masked_data.sql
-\i $DIR/sql/post_data.sql
+\i pre_data.sql
+\i masked_data.sql
+\i post_data.sql
 EOF
